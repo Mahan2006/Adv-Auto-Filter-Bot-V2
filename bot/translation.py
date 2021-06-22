7126e0268f97760161f295c3dc69f2bf071b9208
Mahan M.V <73701328+Mahan2006@users.noreply.github.com>
2021-06-22 19:25:24 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -54,5 +54,5 @@ For More Details Click Help Button Below..
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://t.me/😤
+<b>➥ Source Code</b> : <i><a href="😤
 """
