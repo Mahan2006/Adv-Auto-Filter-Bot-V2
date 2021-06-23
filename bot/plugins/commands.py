dc23f138833d10f87f3823ef513a355659059ba5
The Style King <78583783+Style-King@users.noreply.github.com>
2021-06-23 17:12:05 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -23,7 +23,7 @@ async def start(bot, update):
         if (file_id or file_type) == None:
             return
         
-        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + "</code>")
+        caption = """Nice to meet you here 💞\n\n✯ ━━━━━━━ ✧ ━━━━━━━━ ✯\n➩ Group : @MahanMVGroup\n➩Channel :@MahanMVMovies\n✯ ━━━━━━━ ✧ ━━━━━━━━ ✯"""
         
         if file_type == "document":
         
